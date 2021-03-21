b36825118e8109aa6b3644d0723648665103668a
Khaznaji <oumaymalyna.khaznaji@esprit.tn>
2021-03-21 19:24:31 +0100

    Crud

---
+++ Desktop/PROJETQT/Membre.h
@@ -0,0 +1,34 @@
+#ifndef MEMBRE_H
+#define MEMBRE_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+class Membre
+{public:
+    Membre();
+    Membre(int,int,int,QString,QString,QString,QString);
+
+    QString get_nom();
+    QString get_prenom();
+    QString get_adr();
+    QString get_mail();
+    int get_id();
+    int get_slr();
+    int get_num();
+    void set_nom(QString);
+    void set_prenom(QString);
+    void set_mail(QString);
+    void set_num(int);
+    void set_id(int);
+    void set_adr(QString);
+
+    bool ajouter();
+    QSqlQueryModel * afficher();
+    bool supprimer(int);
+    bool modifier(int,int,int,QString,QString,QString,QString);
+private:
+    QString nom,prenom,adr,mail;
+    int id,num,slr;
+};
+
+#endif // MEMBRE_H
